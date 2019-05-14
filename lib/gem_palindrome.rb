9439011d4ca82a42e2377dea0f7882767a247606
Paulina <paulina.recko@gmail.com>
2019-05-14 14:03:32 +0200

    Upgrade

--- lib/gem_palindrome.rb
+++ lib/gem_palindrome.rb
@@ -1,15 +1,27 @@
 require "gem_palindrome/version"
 
-class String
+module GemPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+  include GemPalindrome
+end
+
+class Integer
+  include GemPalindrome
 end
